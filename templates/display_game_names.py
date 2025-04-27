115c770c441e1c26674560e52c7fa6dfdd580cf8
RSUCSD <ronnysusanto@hotmail.com>
2025-04-26 20:39:34 -0500

    Create display_game_names.py

---
+++ templates/display_game_names.py
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Display Game Name</title>
+</head>
+<body>
+    <h1>You Entered:</h1>
+    <p>{{ game_name }}</p>
+    <a href="/">Go back</a>
+</body>
+</html>
